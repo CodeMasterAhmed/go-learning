6f5ceebdebd57b50b4ffebd5a0b2cac88f5d867c
CodeMasterAhmed <ahmedraza6305@gmail.com>
2024-11-11 13:13:42 +0000

    Updated HTTP Server to Server Static files

--- go programs/httpserver.go
+++ go programs/httpserver.go
@@ -1,18 +1,17 @@
 package main
 
 import (
-    "fmt"
-    "net/http"
+	"fmt"
+	"net/http"
 )
 
-func helloHandler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Hello, World!")
-}
-
 func main() {
-    http.HandleFunc("/", helloHandler)
-    fmt.Println("Starting server on :8080...")
-    if err := http.ListenAndServe(":8080", nil); err != nil {
-        fmt.Println("Server failed to start:", err)
-    }
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Welcome to my website!")
+	})
+
+	fs := http.FileServer(http.Dir("static/"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
+
+	http.ListenAndServe(":80", nil)
 }
